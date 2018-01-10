d1f955b33e5e8b09eed1f74b588ec96505dddedf
Mate Wolf <Mate_Wolf@epam.com>
2018-01-10 09:53:38 +0100

    Initial commit

---
+++ src/main/java/com/vw/bench/epam/vwmandmsapp/VwMandmsAppApplication.java
@@ -0,0 +1,12 @@
+package com.vw.bench.epam.vwmandmsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VwMandmsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VwMandmsAppApplication.class, args);
+	}
+}
